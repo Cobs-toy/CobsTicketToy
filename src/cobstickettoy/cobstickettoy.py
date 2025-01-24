b6f5cca8e8cd95efe71ade963f1fc9da4d523fbb
jacob-53 <jacob8753@gmail.com>
2025-01-24 18:43:56 +0900

    pytest

--- src/cobstickettoy/cobstickettoy.py
+++ src/cobstickettoy/cobstickettoy.py
@@ -1,2 +1,10 @@
-def ticket():
-    return hi
+import typer
+import random
+
+def today_num(i: int,k:int):
+    a = random.randint(i,k)
+    return a
+    print(a)
+
+def entry_point():
+    typer.run(today_num)    
