7c825db3c80da46490c6cbd1d4366eba31746200
jacob-53 <jacob8753@gmail.com>
2025-01-29 23:40:33 +0900

    ajcount

--- src/cobstickettoy/cobstickettoy.py
+++ src/cobstickettoy/cobstickettoy.py
@@ -1,10 +1,19 @@
+import pandas as pd
 import typer
-import random
 
-def today_num(i: int,k:int):
-    a = random.randint(i,k)
-    print(a)
-    return a
+
+def aj_count(keyword: str , asorde : bool ):
+    
+    df = pd.read_csv("data/AJ.csv")
+    df['name']=df['name'].astype(str).str.strip()
+    fdf = df[df['name'].str.contains(keyword,case=False,na=False)]
+    sdf = fdf.sort_values(by="average_sale_price", ascending=asorde).reset_index(drop=True)
+    return sdf
+
+def print_aj_count(keyword: str, asorde : bool ):
+
+    sdf = aj_count(keyword, asorde)
+    print(sdf.to_string(index=False))
 
 def entry_point():
-    typer.run(today_num)    
+    typer.run(print_aj_count)
