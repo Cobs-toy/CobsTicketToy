75bb80a14e41b357d46b5739e8b8ac01ce0c9197
jacob-53 <jacob8753@gmail.com>
2025-02-09 18:10:27 +0900

    v0.3.0

--- src/cobstickettoy/cobstickettoy.py
+++ src/cobstickettoy/cobstickettoy.py
@@ -4,9 +4,9 @@ import typer
 
 def aj_count(keyword: str , asorde : bool, wotby: str ):
     
-    df = pd.read_csv("data/AJ.csv")
-    df['name']=df['name'].astype(str).str.strip()
-    fdf = df[df['name'].str.contains(keyword,case=False,na=False)]
+    df = pd.read_csv("data/AJs.csv")
+    df['Name']=df['Name'].astype(str).str.strip()
+    fdf = df[df['Name'].str.contains(keyword,case=False,na=False)]
     sdf = fdf.sort_values(by=wotby, ascending=asorde).reset_index(drop=True)
     return sdf
 
